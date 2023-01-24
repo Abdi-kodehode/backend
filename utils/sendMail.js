1faa35fa20c4ec04883179943524736a6dd0097e
Abdi Fatah Hersi <101109212+Abdi-kodehode@users.noreply.github.com>
2023-01-24 17:58:56 +0100

    config change

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -3,9 +3,9 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (email, subject, html) => {
     try {
         const transporter = nodemailer.createTransport({
-            host: process.env.HOST,
-            port: 587,
-            secure: false,
+            service: process.env.SERVICE,
+            // port: 587,
+            secure: true,
             auth: {
                 user: process.env.USER,
                 pass: process.env.PASS,
